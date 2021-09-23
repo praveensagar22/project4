c803ae8e660ebb9bca9312f34f317bd107276430
praveensagar22 <74481943+praveensagar22@users.noreply.github.com>
2021-09-23 19:31:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+
+
+  document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+    var button=this.innerHTML;
+    makeSound(button);
+    buttonanimation(button);
+
+  });
+}
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  buttonanimation(event.key);
+
+});
+function makeSound(k){
+
+  switch (k) {
+    case "w":
+    var sound=new Audio("sounds/tom-1.mp3");
+    sound.play();
+
+      break;
+    case "a":
+    var sound1=new Audio("sounds/tom-2.mp3");
+    sound1.play();
+    break;
+
+    case "s":
+    var sound2=new Audio("sounds/tom-3.mp3");
+    sound2.play();
+    break;
+    case "d":
+    var sound3=new Audio("sounds/tom-4.mp3");
+    sound3.play();
+    break;
+    case "j":
+    var sound4=new Audio("sounds/crash.mp3");
+    sound4.play();
+    break;
+    case "k":
+    var sound5=new Audio("sounds/kick-bass.mp3");
+    sound5.play();
+    break;
+    case "l":
+    var sound6=new Audio("sounds/snare.mp3");
+    sound6.play();
+    break;
+}
+}
+function buttonanimation(k){
+  var buttonactive=document.querySelector("."+k);
+  buttonactive.classList.add("pressed");
+  setTimeout(function(){
+    buttonactive.classList.remove("pressed");
+  },100);
+}
+// var sound=new Audio("sounds/tom-1.mp3");
+// sound.play();
